e8cc68aa8cb72373d87fc3663f991c209e3f4d2a
Illia Savinov <dresar2404@gmail.com>
2026-04-13 21:18:15 +0200

    solution

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -1,13 +1,11 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
+
 class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
     def has_permission(self, request, view):
-        # staff може все
         if request.user and request.user.is_staff:
             return True
-        # non-staff не можуть робити DELETE
         if request.method == "DELETE":
             return False
-        # авторизовані — тільки read (SAFE_METHODS)
-        return request.user and request.user.is_authenticated and request.method in SAFE_METHODS
-
+        return (request.user and request.user.is_authenticated
+                and request.method in SAFE_METHODS)
